811725952af29d2e43b31fab93bb00d4595ae4e3
mkaply <mkaply@0c6a3a7e-0c16-11df-a049-0325cb408e29>
2010-01-28 14:07:10 +0000

    Initial checkin of CCK into Google Code
    git-svn-id: http://ff-cckwizard.googlecode.com/svn/trunk@2 0c6a3a7e-0c16-11df-a049-0325cb408e29

---
+++ buildit.sh
@@ -0,0 +1,22 @@
+EXTENSION=cckwizard
+rm  *.xpi
+rm -rf $EXTENSION
+mkdir $EXTENSION
+cd $EXTENSION
+rsync -r --exclude=.svn --exclude-from=../excludefile.txt ../* .
+rm chrome.manifest
+rm chrome.manifest.flat
+mv chrome.manifest.jar chrome.manifest
+cd chrome
+zip -r $EXTENSION.jar content locale skin
+rm -rf content
+rm -rf locale
+rm -rf skin
+cd ../..
+cd $EXTENSION
+#VERSION=`grep "em:version" install.rdf | sed -e 's/[ \t]*em:version=//;s/"//g'`
+VERSION=`grep "em:version" install.rdf | sed -e 's/^[ \t]*//g' | sed -e 's/<[^>]*>//g'`
+XPINAME=$EXTENSION-$VERSION
+zip -r -D ../$XPINAME.xpi *
+cd ..
+#rm -rf $EXTENSION
