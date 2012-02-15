1b3246d9741f39b84f7f67ed5892cc320f21fe74
mkaply@gmail.com <mkaply@gmail.com@0c6a3a7e-0c16-11df-a049-0325cb408e29>
2012-02-15 16:46:08 +0000

    Don't use JAR for CCK either
    git-svn-id: http://ff-cckwizard.googlecode.com/svn/trunk@69 0c6a3a7e-0c16-11df-a049-0325cb408e29

--- buildit.sh
+++ buildit.sh
@@ -8,17 +8,8 @@ rsync -r --exclude=.svn --exclude-from=../excludefile.txt ../* .
 #VERSION=`grep "em:version" install.rdf | sed -e 's/[ \t]*em:version=//;s/"//g'`
 VERSION=`grep "em:version" install.rdf | sed -e 's/[ \t]*<em:version>//;s/<\/em:version>//g'`
 export VERSION=$VERSION
-perl -pi -e 's/chrome\/skin/jar:chrome\/$ENV{"SHORTNAME"}.jar!\/skin/gi' chrome.manifest
-perl -pi -e 's/chrome\/content/jar:chrome\/$ENV{"SHORTNAME"}.jar!\/content/gi' chrome.manifest
-perl -pi -e 's/chrome\/locale/jar:chrome\/$ENV{"SHORTNAME"}.jar!\/locale/gi' chrome.manifest
-cd chrome
-perl -pi -e 's/0.0.0/$ENV{"VERSION"}/gi' content/*.js
-zip -r $SHORTNAME.jar content locale skin
-rm -rf content
-rm -rf locale
-rm -rf skin
-cd ../..
-cd $SHORTNAME
+perl -pi -e 's/0.0.0/$ENV{"VERSION"}/gi' chrome/content/cckWizardOverlay.js
+rm chrome/content/cckWizardOverlay.js.bak
 zip -r -D ../$SHORTNAME-$VERSION.xpi *
 cd ..
 rm -rf $SHORTNAME
